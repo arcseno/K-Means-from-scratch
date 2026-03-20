c90531d22da8f79f7f7ede65be181d34434c0cf2
arcseno <torpaguxosu@gmail.com>
2026-03-19 21:23:34 -0300

    Finished the implementation of the K-Means algorithm. Now, i'm going to spend some time wr

--- KMeans.py
+++ KMeans.py
@@ -11,16 +11,17 @@ Gustavo Taiji Naozuka
 -> O Método fit_predict(x) deve implementar o agrupamento de dados com o
 K-Means. Como fazer isso?
 
-    * Deve realizar a inicialização aleatória, escolhendo K pontos (baseado no n_clusters) do conjunto de dados X para serem os centroides iniciais. Aqui dá pra utilizar o random choice do numpy.
+    * Deve realizar a inicialização aleatória, escolhendo K pontos (baseado no n_clusters) do conjunto de dados X para serem os centroides iniciais. Aqui dá pra utilizar o random choice do numpy. (FEITO)
 
-    * Depois, para cada ponto do conjunto de dados X, nós calculamos a distância euclidiana em relação a cada um dos K centroides.
+    * Depois, para cada ponto do conjunto de dados X, nós calculamos a distância euclidiana em relação a cada um dos K centroides. (FEITO)
     Assim, atribui o ponto ao centroide mais proximo. 
 
-    * Logo após, para cada grupo criado na etapa anterior, calcular a média aritmética das coordenadas de todos os pontos que pertencem a ele. E, finalmente, mover o centroide desse grupo para a nova posição média desse cálculo.
+    * Logo após, para cada grupo criado na etapa anterior, calcular a média aritmética das coordenadas de todos os pontos que pertencem a ele. E, finalmente, mover o centroide desse grupo para a nova posição média desse cálculo. (FEITO)
 
     * O critério de parada, a dita convergência, acontecerá quando:
         -> Os centroides pararam de se mover (a diferença entre a posição antiga e a nova é < que a tolerância dada)
         -> O número máximo de repetições max_iter for atingido.
+        (CONFERIDO).
 
 2º STEP:
 
@@ -33,11 +34,6 @@ de Clientes de Academia.
 cluster encontrado, e devem ter os centroides finais (após convergir) com um 
 símbolo diferente.
 
-ENTREGA:
-
--> Dois arquivos, um KMeans.py e uma main.py
--> Compactar ambos e entregar em um arquivo KMeans_ViniciusOya.zip
-
 ---------------------------------------------------------------------------
 
 '''
@@ -45,21 +41,47 @@ ENTREGA:
 import numpy as np
 
 class KMeans:
-    def __init__(self, n_clusters=3, max_iter=300, tol=0.0001):
+    def __init__(self, n_clusters=2, max_iter=300, tol=0.0001):
         self.n_clusters = n_clusters
         self.max_iter = max_iter
         self.tol = tol
         self.centroids = None
 
     def fit_predict(self, X):
-        # Etapa de inicialização randômica dos centróides:
-        indices = np.random.choice(len(X), self.n_clusters, replace=False)
-        self.centroids = X[indices]
+        # Sorteia, inicialmente, os centroides iniciais
+        indices_aleatorios = np.random.choice(len(X), self.n_clusters, replace=False)
+        self.centroids = X[indices_aleatorios].astype(float)
 
-        # O limite máximo de iteração é o hiperparametro de max_iter, e vai acabar ou ao final do loop, ou qnd convergir de acordo com a tolerância dada
-        for i in range(self.max_iter):
+        for iteracao in range(self.max_iter):
+            labels = []
 
-        
-    
-        #return labels
+            # Aqui, vamos calcular a distância e encontrar o grupo mais perto
+            for ponto in X:
+                distancias = [np.linalg.norm(ponto - centroide) for centroide in self.centroids]
+                grupo_mais_perto = np.argmin(distancias)
+                labels.append(grupo_mais_perto)
+
+            labels = np.array(labels)
+            novos_centroides = []
+
+            # Ainda dentro do loop, vamos calcular os novos centroides
+            for grupo in range(self.n_clusters):
+                pontos_do_grupo = X[labels == grupo]
+                if len(pontos_do_grupo) > 0:
+                    media = np.mean(pontos_do_grupo, axis=0)
+                    novos_centroides.append(media)
+                else:
+                    novos_centroides.append(self.centroids[grupo])
+
+            novos_centroides = np.array(novos_centroides)
+            
+            deslocamento = np.linalg.norm(novos_centroides - self.centroids)
+            
+            # Parte da decisão da convergência
+            if deslocamento < self.tol:
+                break
+
+            self.centroids = novos_centroides
+
+        return labels
 
--- main.py
+++ main.py
@@ -1,2 +1,75 @@
 from KMeans import KMeans
+import matplotlib.pyplot as plt
+import numpy as np
 
+# Conjunto de dados do exemplo: Segmentação de Clientes de Academia.
+# Formato: (IMC, Frequência Semanal de Exercícios)
+
+X = np.array([
+    [24, 2],
+    [27, 3],
+    [23, 1],
+    [30, 5],
+    [22, 2],
+    [33, 6],
+    [31, 5],
+    [29, 4]
+])
+
+# -------------------------------------------------- #
+# Implementação do Método do Cotovelo (Elbow Method) #
+# -------------------------------------------------- #
+# Aqui, calcularemos o WCSS (Soma dos Quadrados Intra-Cluster), e faremos o resto abaixo:
+
+def calcular_wcss(X, labels, centroides):
+    soma_dos_erros = 0
+    for i in range(len(X)):
+        ponto = X[i]
+        cluster_atual = labels[i]
+        centroide_atual = centroides[cluster_atual]
+        
+        # Calcula a distancia do cliente até o "X" do grupo dele e eleva ao quadrado
+        distancia = np.linalg.norm(ponto - centroide_atual)
+        soma_dos_erros += distancia ** 2
+        
+    return soma_dos_erros
+
+# Com a função pronta, começamos a calcular o WCSS para cada valor de K, e depois plotamos o gráfico do método do cotovelo.
+
+valores_wcss = []
+k_tentativas = range(1,8)
+
+for k in k_tentativas:
+    np.random.seed(42)
+    kmeans = KMeans(n_clusters=k, max_iter=300, tol=0.0001)
+    labels_cotovelo = kmeans.fit_predict(X)
+
+    erro = calcular_wcss(X, labels_cotovelo, kmeans.centroids)
+    valores_wcss.append(erro)
+
+plt.figure()
+plt.plot(k_tentativas, valores_wcss, marker='o', color='blue')
+plt.title('Elbow Method')
+plt.xlabel('Number of Clusters (K)')
+plt.ylabel('WCSS (Error)')
+plt.savefig('elbow_method.png')
+plt.show()
+
+# ----------------------------------------------------------- #
+# Gráfico de dispersão com os clusters e os centroides finais #
+# ----------------------------------------------------------- #
+# Dada a etapa anterior, chegamos na conclusão de que a quantidade ideal para K é 2. Agora, rodamos o modelo.
+
+np.random.seed(42)
+kmeans_final = KMeans(n_clusters=2, max_iter=300, tol=0.0001)
+labels_finais = kmeans_final.fit_predict(X)
+centroides_final = kmeans_final.centroids
+
+plt.figure()
+plt.scatter(X[:, 0], X[:, 1], c=labels_finais)
+plt.scatter(centroides_final[:, 0], centroides_final[:, 1], c='red', marker='x')
+plt.title('Gym Customer Segmentation')
+plt.xlabel('BMI')
+plt.ylabel('Weekly Frequency')
+plt.savefig('kmeans_plot.png')
+plt.show()
